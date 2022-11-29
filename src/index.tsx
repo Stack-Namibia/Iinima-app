9cb08dabf121a7b203fe6ff6422b408f472bdce5
Hero <hherobiam@gmail.com>
2022-11-29 15:56:37 +0200

    A-76 navigation-bar (#1)
    * A-76 navigation-bar
    
    * Few fixes
    
    * Revert "Few fixes"
    
    This reverts commit 7ac08abc9752912d439575c2db9d280de2960958.
    
    * Few fixes
    
    * updates
    
    * updates on navigation bar
    
    * IA-67 Add Daisy UI Navigation
    
    * IA- Add MUI Navigation
    
    Co-authored-by: Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import NavigationBar from "./components/general/NavigationBar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <NavigationBar />
     <App />
   </BrowserRouter>
 );
