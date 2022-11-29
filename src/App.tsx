ce7d7e27fb60691d1cbeea7e6836c097a7ab7394
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-11-29 11:48:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/pages/Home";
 import routes from "./settings/routes";
 
 function App() {
