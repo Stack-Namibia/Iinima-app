93f3316b81ef0479eb51d6d67463ac6874dfd4cf
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-11-29 23:48:02 +0200

    Move to page folder

--- src/components/pages/profile/index.tsx
+++ src/components/pages/profile/index.tsx
@@ -5,7 +5,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import ApplicationWrapper from "../general/ApplicationWrapper";
+import ApplicationWrapper from "../../general/ApplicationWrapper";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -18,7 +18,7 @@ function TabPanel(props: TabPanelProps) {
 
   return (
     <div
-      role="tabpanel"
+      role='tabpanel'
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
@@ -69,54 +69,54 @@ const Profile = () => {
   return (
     <>
       <ApplicationWrapper>
-        <div className="flex h-screen">
-          <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
-            <div className="lg:hidden">
-              <div className="flex items-center justify-between border-b border-gray-200 bg-gray-50 px-4 py-1.5"></div>
+        <div className='flex h-screen'>
+          <div className='flex min-w-0 flex-1 flex-col overflow-hidden'>
+            <div className='lg:hidden'>
+              <div className='flex items-center justify-between border-b border-gray-200 bg-gray-50 px-4 py-1.5'></div>
             </div>
-            <div className="relative z-0 flex flex-1 overflow-hidden">
-              <main className="relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last">
+            <div className='relative z-0 flex flex-1 overflow-hidden'>
+              <main className='relative z-0 flex-1 overflow-y-auto focus:outline-none xl:order-last'>
                 <article>
                   {/* Profile header */}
                   <div>
                     <div>
                       <img
-                        className="h-32 w-full object-cover lg:h-48"
+                        className='h-32 w-full object-cover lg:h-48'
                         src={profile.coverImageUrl}
-                        alt=""
+                        alt=''
                       />
                     </div>
-                    <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-                      <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-                        <div className="flex">
+                    <div className='mx-auto max-w-5xl px-4 sm:px-6 lg:px-8'>
+                      <div className='-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5'>
+                        <div className='flex'>
                           <img
-                            className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                            className='h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32'
                             src={profile.imageUrl}
-                            alt=""
+                            alt=''
                           />
                         </div>
-                        <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-                          <div className="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
-                            <Name className="truncate text-2xl font-bold text-gray-900">
+                        <div className='mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1'>
+                          <div className='mt-6 min-w-0 flex-1 sm:hidden 2xl:block'>
+                            <Name className='truncate text-2xl font-bold text-gray-900'>
                               {profile.name}
                             </Name>
                           </div>
-                          <div className="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                          <div className='justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4'>
                             <EditButton
-                              type="button"
-                              className="inline-flex justify-center rounded-3xl border border-gray-300 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                              type='button'
+                              className='inline-flex justify-center rounded-3xl border border-gray-300 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2'
                             >
                               <PencilIcon
-                                className="-ml-1 mr-2 h-5 w-5"
-                                aria-hidden="true"
+                                className='-ml-1 mr-2 h-5 w-5'
+                                aria-hidden='true'
                               />
                               <span>Edit Profile</span>
                             </EditButton>
                           </div>
                         </div>
                       </div>
-                      <div className="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
-                        <Name className="truncate text-2xl font-bold text-gray-900">
+                      <div className='mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden'>
+                        <Name className='truncate text-2xl font-bold text-gray-900'>
                           {profile.name}
                         </Name>
                       </div>
@@ -124,14 +124,14 @@ const Profile = () => {
                   </div>
 
                   {/* Tabs */}
-                  <div className="mt-6 sm:mt-2 2xl:mt-5">
-                    <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                  <div className='mt-6 sm:mt-2 2xl:mt-5'>
+                    <div className='mx-auto max-w-5xl px-4 sm:px-6 lg:px-8'>
                       <Box sx={{ width: "100%" }}>
                         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                           <Tabs
                             value={value}
                             onChange={handleChange}
-                            aria-label="basic tabs example"
+                            aria-label='basic tabs example'
                             sx={{
                               "& .MuiTabs-indicator": {
                                 backgroundColor: "#d63e3e;",
@@ -148,29 +148,29 @@ const Profile = () => {
                               },
                             }}
                           >
-                            <Tab label="Profile" {...a11yProps(0)} />
-                            <Tab label="Listed items" {...a11yProps(1)} />
-                            <Tab label="Favourites" {...a11yProps(2)} />
+                            <Tab label='Profile' {...a11yProps(0)} />
+                            <Tab label='Listed items' {...a11yProps(1)} />
+                            <Tab label='Favourites' {...a11yProps(2)} />
                           </Tabs>
                         </Box>
                         <TabPanel value={value} index={0}>
-                          <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                          <dl className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2'>
                             {Object.keys(profile.fields).map((field) => (
-                              <div key={field} className="sm:col-span-1">
-                                <dt className="text-sm font-medium text-gray-500">
+                              <div key={field} className='sm:col-span-1'>
+                                <dt className='text-sm font-medium text-gray-500'>
                                   {field}
                                 </dt>
-                                <dd className="mt-1 text-sm text-gray-900">
+                                <dd className='mt-1 text-sm text-gray-900'>
                                   {profile.fields[field]}
                                 </dd>
                               </div>
                             ))}
-                            <div className="sm:col-span-2">
-                              <dt className="text-sm font-medium text-gray-500">
+                            <div className='sm:col-span-2'>
+                              <dt className='text-sm font-medium text-gray-500'>
                                 About
                               </dt>
                               <dd
-                                className="mt-1 max-w-prose space-y-5 text-sm text-gray-900"
+                                className='mt-1 max-w-prose space-y-5 text-sm text-gray-900'
                                 dangerouslySetInnerHTML={{
                                   __html: profile.about,
                                 }}
--- src/settings/routes.tsx
+++ src/settings/routes.tsx
@@ -3,7 +3,7 @@ import Home from "../components/pages/Home";
 import HowItWorks from "../components/pages/HowItWorks";
 import ListItem from "../components/pages/items/list-items";
 import HttpError from "../components/pages/http-error";
-import Profile from "../components/pages/Profile";
+import Profile from "../components/pages/profile";
 import SignIn from "../components/pages/sign-in";
 import SignUp from "../components/pages/sign-up";
 
