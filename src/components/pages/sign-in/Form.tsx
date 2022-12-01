71ae66de95badd082497763f3cf6071b58573020
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-12-01 20:43:52 +0200

    IA 78 wire up sign in components functionality (#15)
    * IA-79 Add Inputs functionality
    
    * IA-78 Add submit logic

--- src/components/pages/sign-in/Form.tsx
+++ src/components/pages/sign-in/Form.tsx
@@ -1,55 +1,137 @@
-import { Facebook, Twitter, Google } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-import { Input } from "../../general/Input";
+import {
+  Facebook,
+  Twitter,
+  Google,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
 import { Button } from "../../general/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const Form = () => (
-  <div className='flex items-center justify-center p-6 sm:p-12 md:w-1/2'>
-    <div className='w-full'>
-      <div className='flex justify-center'>
-        <img src='/logo 1.svg' alt='logo' className='w-[60%]' />
-      </div>
-      <h1 className='mb-4 text-2xl font-bold text-center text-gray-700'>
-        Login to Your Account
-      </h1>
-      <div className='mb-2'>
-        <Input id='email' label='Email' type='email' />
-      </div>
-      <div>
-        <Input id='password' label='Password' type='password' />
-      </div>
-      <p className='mt-4 font-semibold text-sm'>
-        <Link className='text-sm text-primary hover:underline' to={"/signin"}>
-          Forgot your password?
-        </Link>
-      </p>
-      <Button text='Sign In' />
-      <p className='mt-2 font-semibold text-[#7E7A7A] text-sm'>
-        Don't have an account?{" "}
-        <span className='text-sm text-primary hover:underline'>
-          <Link to={"/signup"}>Register</Link>
-        </span>
-      </p>
+const TextFieldProps: any = {
+  variant: "outlined",
+  fullWidth: true,
+  margin: "normal",
+};
 
-      <div className='divider'>Or continue with</div>
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-      <div className='flex items-center justify-center gap-4'>
-        <IconButton>
-          <Facebook
-            fontSize='large'
-            className='text-primary hover:text-black'
-          />
-        </IconButton>
-        <IconButton>
-          <Twitter fontSize='large' className='text-primary hover:text-black' />
-        </IconButton>
-        <IconButton>
-          <Google fontSize='large' className='text-primary hover:text-black' />
-        </IconButton>
+  const clearData = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { email, password };
+    console.log(data);
+
+    setLoading(true);
+    //Replace with API call to authenticate user
+    setTimeout(() => {
+      setLoading(false);
+      clearData();
+    }, 5000);
+  };
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+  return (
+    <div className='flex items-center justify-center p-6 sm:p-12 md:w-1/2'>
+      <div className='w-full'>
+        <div className='flex justify-center'>
+          <img src='/logo 1.svg' alt='logo' className='w-[60%]' />
+        </div>
+        <h1 className='mb-4 text-2xl font-bold text-center text-gray-700'>
+          Login to Your Account
+        </h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className='mb-2'>
+            <TextField
+              {...TextFieldProps}
+              id='email'
+              label='Email'
+              type='email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <TextField
+              {...TextFieldProps}
+              id='password'
+              label='Password'
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={() => setShowPassword(!showPassword)}
+                      onMouseDown={handleMouseDownPassword}
+                      edge='end'
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <p className='mt-4 font-semibold text-sm'>
+            <Link
+              className='text-sm text-primary hover:underline'
+              to={"/signin"}
+            >
+              Forgot your password?
+            </Link>
+          </p>
+          <Button text='Sign In' type='submit' loading={loading} />
+        </form>
+        <p className='mt-2 font-semibold text-[#7E7A7A] text-sm'>
+          Don't have an account?{" "}
+          <span className='text-sm text-primary hover:underline'>
+            <Link to={"/signup"}>Register</Link>
+          </span>
+        </p>
+
+        <div className='divider'>Or continue with</div>
+
+        <div className='flex items-center justify-center gap-4'>
+          <IconButton>
+            <Facebook
+              fontSize='large'
+              className='text-primary hover:text-black'
+            />
+          </IconButton>
+          <IconButton>
+            <Twitter
+              fontSize='large'
+              className='text-primary hover:text-black'
+            />
+          </IconButton>
+          <IconButton>
+            <Google
+              fontSize='large'
+              className='text-primary hover:text-black'
+            />
+          </IconButton>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Form;
