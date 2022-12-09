463409ebfae77a532d2484c0b5f0bddd67dc3f78
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-12-09 11:47:34 +0200

    IA-83 Add user actions

--- src/store/actions/user-actions.ts
+++ src/store/actions/user-actions.ts
@@ -1,1 +1,14 @@
-export {};
+import { User } from "../../api/accounts";
+import { UserActionTypes } from "../action-creators/user-action-creator";
+
+interface GetUserAction {
+  type: UserActionTypes.FETCH_USER;
+  payload: User;
+}
+
+interface SetUserAction {
+  type: UserActionTypes.SET_USER;
+  payload: User;
+}
+
+export type UserAction = GetUserAction | SetUserAction;
