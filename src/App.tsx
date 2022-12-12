ccc43fdaca11deda2bf27079835ac89c8acaae1f
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-12-12 15:10:32 +0200

    #21 Add animations between pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 import "./App.css";
 import routes from "./settings/routes";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <Switch>
-        {routes.map(({ key, path, element, exact }) => (
-          <Route path={path} key={key} exact>
-            {element}
-          </Route>
-        ))}
-      </Switch>
+      <AnimatePresence>
+        <Switch>
+          {routes.map(({ key, path, element }) => (
+            <Route path={path} key={key} exact location={location}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: "100%" }}
+                exit={{ opacity: 0 }}
+              >
+                {element}
+              </motion.div>
+            </Route>
+          ))}
+        </Switch>
+      </AnimatePresence>
     </>
   );
 }
--- src/components/pages/how-it-works/index.tsx
+++ src/components/pages/how-it-works/index.tsx
@@ -8,18 +8,18 @@ import BrowseItemButton from "../../general/BrowseItemButton";
 const HowItWorks = () => {
   return (
     <HowItWorksWrapper>
-      <div className="flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0">
-        <div className="py-12">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={undraw} className="max-w-sm rounded-lg" alt="img" />
-            <div className="flex flex-col justify-center mt-10 w-full">
-              <h1 className="font-bold text-5xl text-black">
+      <div className='flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0'>
+        <div className='py-12'>
+          <div className='hero-content flex-col lg:flex-row-reverse'>
+            <img src={undraw} className='max-w-sm rounded-lg' alt='img' />
+            <div className='flex flex-col justify-center mt-10 w-full'>
+              <h1 className='font-bold text-5xl text-black'>
                 How to rent anything
               </h1>
-              <h2 className="font-bold text-5xl text-black mt-5 text-right mr-10 text-red-700">
+              <h2 className='font-bold text-5xl text-black mt-5 text-right mr-10 text-red-700'>
                 Anything
               </h2>
-              <p className="text-xl text-black mt-5 pl-10 text-right text-gray-500">
+              <p className='text-xl text-black mt-5 pl-10 text-right text-gray-500'>
                 Access items without owning them by renting them from around
                 your area.
               </p>
@@ -27,32 +27,32 @@ const HowItWorks = () => {
           </div>
         </div>
       </div>
-      <div className="flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0 bg-wave1">
-        <div className="py-12">
-          <div className="hero-content flex-col lg:flex-row">
-            <img src={logo} className="max-w-sm rounded-lg" alt="img" />
-            <div className="flex flex-col justify-center mt-10 w-full">
-              <Headers className="font-bold text-5xl text-black text-red-700">
+      <div className='flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0 bg-wave1'>
+        <div className='py-12'>
+          <div className='hero-content flex-col lg:flex-row'>
+            <img src={logo} className='max-w-sm rounded-lg' alt='img' />
+            <div className='flex flex-col justify-center mt-10 w-full'>
+              <Headers className='font-bold text-5xl text-black text-red-700'>
                 Before rental
               </Headers>
-              <SecondHeaders className="font-bold text-2xl text-black mt-3">
+              <SecondHeaders className='font-bold text-2xl text-black mt-3'>
                 Find an item close to you
               </SecondHeaders>
-              <p className="text-black mt-1 text-left text-gray-500">
+              <p className='text-black mt-1 text-left text-gray-500'>
                 Search for the items you’re looking for and filter by location.
               </p>
-              <SecondHeaders className="font-bold text-2xl text-black mt-3 whitespace-normal">
+              <SecondHeaders className='font-bold text-2xl text-black mt-3 whitespace-normal'>
                 Request and verify
               </SecondHeaders>
-              <p className="text-black mt-1 text-right text-gray-500 whitespace-normal">
+              <p className='text-black mt-1 text-right text-gray-500 whitespace-normal'>
                 Send a request to the lender for the dates you’d like the items.
                 Your booking is only confirmed when the owner accepts your
                 request.
               </p>
-              <SecondHeaders className="font-bold text-2xl text-black mt-3">
+              <SecondHeaders className='font-bold text-2xl text-black mt-3'>
                 Arrange pick up with the owner
               </SecondHeaders>
-              <p className="text-black mt-1 text-left text-gray-500 whitespace-normal">
+              <p className='text-black mt-1 text-left text-gray-500 whitespace-normal'>
                 Use chat to arrange convenient times to pick up the item and ask
                 any questions you might have.
               </p>
@@ -64,25 +64,25 @@ const HowItWorks = () => {
         </div>
       </div>
 
-      <div className="flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0">
-        <div className="py-12">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={logo} className="max-w-sm rounded-lg" alt="img" />
-            <div className="flex flex-col justify-center mt-10 w-full">
-              <Headers className="font-bold text-5xl text-black text-red-700">
+      <div className='flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0'>
+        <div className='py-12'>
+          <div className='hero-content flex-col lg:flex-row-reverse'>
+            <img src={logo} className='max-w-sm rounded-lg' alt='img' />
+            <div className='flex flex-col justify-center mt-10 w-full'>
+              <Headers className='font-bold text-5xl text-black text-red-700'>
                 During rental
               </Headers>
-              <SecondHeaders className="font-bold text-2xl text-black mt-3">
+              <SecondHeaders className='font-bold text-2xl text-black mt-3'>
                 Enjoy your rental and return on time
               </SecondHeaders>
-              <p className="text-black mt-1 text-left text-gray-500">
+              <p className='text-black mt-1 text-left text-gray-500'>
                 Make the most of your time with the item and then return it
                 safely to the owner at the end of the rental.
               </p>
-              <SecondHeaders className="font-bold text-2xl text-black mt-3 whitespace-normal">
+              <SecondHeaders className='font-bold text-2xl text-black mt-3 whitespace-normal'>
                 Need more time?
               </SecondHeaders>
-              <p className="text-black mt-1 text-right text-gray-500 whitespace-normal">
+              <p className='text-black mt-1 text-right text-gray-500 whitespace-normal'>
                 Be sure to check in with the lender and book extra days if the
                 item is available and you want to keep it for longer.
               </p>
@@ -94,15 +94,15 @@ const HowItWorks = () => {
         </div>
       </div>
 
-      <div className="flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0 bg-wave2">
-        <div className="py-12">
-          <div className="hero-content flex-col lg:flex-row">
-            <img src={logo} className="max-w-sm rounded-lg" alt="img" />
-            <div className="flex flex-col justify-center mt-10 w-full">
-              <Headers className="font-bold text-5xl text-black text-red-700 mb-5">
+      <div className='flex-row justify-center max-w-full bg-white rounded-lg mb-0 md:flex md:items-center mr-0 ml-0 bg-wave2'>
+        <div className='py-12'>
+          <div className='hero-content flex-col lg:flex-row'>
+            <img src={logo} className='max-w-sm rounded-lg' alt='img' />
+            <div className='flex flex-col justify-center mt-10 w-full'>
+              <Headers className='font-bold text-5xl text-black text-red-700 mb-5'>
                 Playing your part
               </Headers>
-              <p className="text-black mt-1 text-left text-gray-500 mt-5">
+              <p className='text-black mt-1 text-left text-gray-500 mt-5'>
                 Renting on Iinima isn’t just convenient and cost effective. By
                 buying less and renting more, you’re also contributing to a
                 circular economy, making better use of resources and helping to
