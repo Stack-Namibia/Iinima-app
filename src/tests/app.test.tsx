dd6416517e2e771cb6aaac63dde25ba0d63e1e86
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2022-12-12 17:26:00 +0200

    Fix tests

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -2,11 +2,15 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../store";
 
 test("Loads home", async () => {
   render(
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   );
 });
