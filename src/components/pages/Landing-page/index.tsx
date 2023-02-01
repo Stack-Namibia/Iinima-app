7e9b557bf5fd6c8d9e66e3e2ccfaf287ec6e18cf
Pride Simbotwe <pridemaswahu@gmail.com>
2023-02-01 21:48:32 +0200

    Fixes issue #34 (#35)
    * Fixes #34
    
    * Update index.tsx

--- src/components/pages/Landing-page/index.tsx
+++ src/components/pages/Landing-page/index.tsx
@@ -53,66 +53,62 @@ const Home = () => {
 
   return (
     <HowItWorksWrapper>
-      <div className='hero min-h-screen bg-white'>
-        <div className='hero-content flex-col lg:flex-row-reverse'>
+      <div className="hero min-h-screen bg-white">
+        <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={logo}
-            className='hidden max-w-sm md:block lg:block'
-            alt='logo'
+            className="hidden max-w-sm md:block lg:block"
+            alt="logo"
           />
           <div>
             <InnerHeading className="flex flex-col lg:flex-row">
-            <p className='font-bold text-7xl text-black mr-5'>
-              Rent
-            </p>
-            <Words className="">
-            <MarkText className="font-bold text-7xl">Anything</MarkText>
-            <MarkText className="font-bold text-7xl">Tools</MarkText>
-            <MarkText className="font-bold text-7xl">Furniture</MarkText>
-            <MarkText className="font-bold text-7xl">Clothing</MarkText>
-            <MarkText className="font-bold text-7xl">Electronics</MarkText>
-            <MarkText className="font-bold text-7xl">Anything</MarkText>
-            </Words>
+              <p className="font-bold text-7xl text-black mr-5">Rent</p>
+              <Words className="">
+                <MarkText className="font-bold text-7xl">Anything</MarkText>
+                <MarkText className="font-bold text-7xl">Tools</MarkText>
+                <MarkText className="font-bold text-7xl">Furniture</MarkText>
+                <MarkText className="font-bold text-7xl">Clothing</MarkText>
+                <MarkText className="font-bold text-7xl">Electronics</MarkText>
+                <MarkText className="font-bold text-7xl">Anything</MarkText>
+              </Words>
             </InnerHeading>
-            
-            <div
-              className='font-bold text-7xl text-black whitespace-pre-line w-3/4 mt-3'
-            >
+
+            <div className="font-bold text-7xl text-black whitespace-pre-line w-3/4">
               From within your area
             </div>
-            <div className='flex flex-row items-center mt-10 lg:flex-row gap-5'>
+            <div className="flex flex-row items-center mt-10 lg:flex-row gap-5">
               <SearchInput data={["Grinder", "Camera"]} />
               <Link to={"/item/browse"}>
-                <Button text='Browse Items' />
+                <Button text="Browse Items" />
               </Link>
             </div>
           </div>
         </div>
       </div>
       <header
-        className='header'
+        className="header"
         style={{
           backgroundImage: `url(${divider})`,
         }}
       >
-        <div className='text-box'>
-          <h1 className='heading-primary'>
-            <span className='font-bold  text-2xl text-black'>Save money</span>
-            <span className='heading-primary-sub text-black'>
+        <div className="text-box">
+          <h1 className="heading-primary">
+            <span className="font-bold  text-2xl text-black">Save money</span>
+            <span className="heading-primary-sub text-black">
               Search for something you need and rent it for some day!
             </span>
           </h1>
-          <Link to='/item/browse'>
-            <Button text='Browse Items' />
+          <Link to="/item/browse">
+            <Button text="Browse Items" />
           </Link>
         </div>
       </header>
-      <div className='hero min-h-[50%] mb-10 mt-10'>
-        <div className='flex-col justify-center max-w-full  rounded-lg mb-5 md:flex md:items-center mr-4 ml-4'>
-          <div className='font-bold text-5xl text-black mb-20 text-center'>
+      <div className="hero min-h-[50%] mb-10 mt-10">
+        <div className="flex-col justify-center max-w-full  rounded-lg mb-5 md:flex md:items-center mr-4 ml-4">
+          <div className="font-bold text-5xl text-black mb-20 text-center">
             <h1>Explore our categories</h1>
           </div>
-          <ul className='grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5 items-center'>
+          <ul className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5 items-center">
             {categories.map((category) => (
               <Link to={"/item/browse"}>
                 <CardList>
@@ -138,34 +134,34 @@ const MarkText = styled.span`
   animation: spin_words 16s 1s ease infinite;
   @keyframes spin_words {
     10% {
-      transform: translateY(-100%)
+      transform: translateY(-100%);
     }
     20% {
-      transform: translateY(-100%)
+      transform: translateY(-100%);
     }
     30% {
-      transform: translateY(-200%)
+      transform: translateY(-200%);
     }
     40% {
-      transform: translateY(-200%)
+      transform: translateY(-200%);
     }
     50% {
-      transform: translateY(-300%)
+      transform: translateY(-300%);
     }
     60% {
-      transform: translateY(-300%)
+      transform: translateY(-300%);
     }
     70% {
-      transform: translateY(-400%)
+      transform: translateY(-400%);
     }
     80% {
-      transform: translateY(-400%)
+      transform: translateY(-400%);
     }
     90% {
-      transform: translateY(-500%)
+      transform: translateY(-500%);
     }
     100% {
-      transform: translateY(-500%)
+      transform: translateY(-500%);
     }
   }
 `;
@@ -180,12 +176,9 @@ const CardList = styled.li`
 const InnerHeading = styled.div`
   box-sizing: box-border;
   display: flex;
-  height: 90px;
-  @media (min-height: 300px) and (max-height: 1000px) {
-    height: 160px;
-}
 `;
 
 const Words = styled.div`
-overflow: hidden;
+  overflow: hidden;
+  height: 85px;
 `;
