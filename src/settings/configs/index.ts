dc555ac413de58736ee809fcb78da991d2afeb3b
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2023-02-23 20:59:51 +0200

    Add api key to .env

--- src/settings/configs/index.ts
+++ src/settings/configs/index.ts
@@ -2,4 +2,5 @@ import auth0 from "./auth0";
 
 export const configs = {
   auth0,
+  apiKey: process.env.REACT_APP_API_KEY,
 };
--- src/utils/firebase/api-config.ts
+++ src/utils/firebase/api-config.ts
@@ -1,7 +1,9 @@
 import { Configuration } from "../../api/accounts";
+import { configs } from "../../settings/configs";
 ///this is the function that returns the configuration object, this will set the header for the api call
 
 export function getApiConfig() {
+  console.log("apikey", configs.apiKey);
   return new Configuration({
     accessToken: () => {
       return sessionStorage.getItem("auth0_token") || "";
@@ -9,6 +11,7 @@ export function getApiConfig() {
     baseOptions: {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem("auth0_token")}`,
+        "x-api-key": configs.apiKey,
       },
     },
   });
