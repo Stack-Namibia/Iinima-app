f8155fbdcd1dd3138f1e1d09f0826ad4e91b0f19
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2023-07-27 09:16:43 +0200

    partial wireup of single items page

--- src/components/pages/items/single-item/index.tsx
+++ src/components/pages/items/single-item/index.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ApplicationWrapper from "../../../general/ApplicationWrapper";
 import { extractUUIDFromString } from "../../../../utils/data";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../store/reducers";
 import { bindActionCreators } from "@reduxjs/toolkit";
 import * as ItemActionsCreator from "../../../../store/action-creators/items-action-creator";
+import { LocationCity, MapRounded, PinDrop } from "@mui/icons-material";
 
 const SingleItem = () => {
   const dispatch = useDispatch();
 
-  const { item, isLoading } = useSelector((state: RootState) => state.items);
+  const { item } = useSelector((state: RootState) => state.items);
 
   const { getItem } = bindActionCreators(ItemActionsCreator, dispatch);
 
@@ -92,108 +93,18 @@ const SingleItem = () => {
 
               <div className='lg:col-span-2 lg:row-span-2 lg:row-end-2'>
                 <h1 className='sm: text-2xl font-bold text-gray-900 sm:text-3xl'>
-                  Afro-Brazillian Coffee
+                  {item?.title}
                 </h1>
 
-                <div className='mt-5 flex items-center'>
-                  <div className='flex items-center'>
-                    <svg
-                      className='block h-4 w-4 align-middle text-yellow-500'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
-                        className=''
-                      ></path>
-                    </svg>
-                    <svg
-                      className='block h-4 w-4 align-middle text-yellow-500'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
-                        className=''
-                      ></path>
-                    </svg>
-                    <svg
-                      className='block h-4 w-4 align-middle text-yellow-500'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
-                        className=''
-                      ></path>
-                    </svg>
-                    <svg
-                      className='block h-4 w-4 align-middle text-yellow-500'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
-                        className=''
-                      ></path>
-                    </svg>
-                    <svg
-                      className='block h-4 w-4 align-middle text-yellow-500'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'
-                        className=''
-                      ></path>
-                    </svg>
-                  </div>
-                  <p className='ml-2 text-sm font-medium text-gray-500'>
-                    1,209 Reviews
-                  </p>
+                <h2 className='mt-8 text-base text-gray-900'>Description</h2>
+                <div className='mt-3 flex select-none flex-wrap items-center gap-1'>
+                  <span>{item?.description}</span>
                 </div>
 
-                <h2 className='mt-8 text-base text-gray-900'>Coffee Type</h2>
+                <h2 className='mt-8 text-base text-gray-900'>Location</h2>
                 <div className='mt-3 flex select-none flex-wrap items-center gap-1'>
-                  <label className=''>
-                    <input
-                      type='radio'
-                      name='type'
-                      value='Powder'
-                      className='peer sr-only'
-                      checked
-                    />
-                    <p className='peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold'>
-                      Powder
-                    </p>
-                  </label>
-                  <label className=''>
-                    <input
-                      type='radio'
-                      name='type'
-                      value='Whole Bean'
-                      className='peer sr-only'
-                    />
-                    <p className='peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold'>
-                      Whole Bean
-                    </p>
-                  </label>
-                  <label className=''>
-                    <input
-                      type='radio'
-                      name='type'
-                      value='Groud'
-                      className='peer sr-only'
-                    />
-                    <p className='peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold'>
-                      Groud
-                    </p>
-                  </label>
+                  <PinDrop className='text-primary' />{" "}
+                  <span>{item?.location}</span>
                 </div>
 
                 <h2 className='mt-8 text-base text-gray-900'>
