7c266ed30ff73b9eb144bf2c87c9116acb53fe64
Carlos Sibalatani <36531734+FearTheBeard95@users.noreply.github.com>
2023-08-01 09:44:21 +0200

    Add item not found errorpage

--- src/components/pages/items/single-item/index.tsx
+++ src/components/pages/items/single-item/index.tsx
@@ -9,6 +9,7 @@ import { useAccount } from "../../../../hooks/accounts/queries";
 import { configs } from "../../../../settings/configs";
 import LoadingPage from "../../loading-page";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import HttpError from "../../http-error";
 
 const SingleItem = () => {
   const { user } = useAuth0();
@@ -21,11 +22,16 @@ const SingleItem = () => {
   const path = window.location.pathname;
   const itemId = extractUUIDFromString(path);
 
-  const { data: item, isLoading: isLoadingItem } = useGetItem(itemId || "");
-  const { data: itemUser, isLoading: isLoadingUser } = useAccount(
-    item?.user_id || "",
-    !!item
-  );
+  const {
+    data: item,
+    isLoading: isLoadingItem,
+    isError: errorGettingItem,
+  } = useGetItem(itemId || "");
+  const {
+    data: itemUser,
+    isLoading: isLoadingUser,
+    isError: errorGettingUser,
+  } = useAccount(item?.user_id || "", !!item);
 
   const [selectedPhoto, setSelectedPhoto] = useState<string>("");
 
@@ -40,6 +46,19 @@ const SingleItem = () => {
 
   if (isLoadingItem || isLoadingUser) {
     return <LoadingPage />;
+  }
+
+  if (errorGettingItem || !item) {
+    return (
+      <HttpError
+        // code='Item not found - 404'
+        message='The item could not be found'
+      />
+    );
+  }
+
+  if (errorGettingUser || !itemUser) {
+    return <HttpError message='Something went wrong. Please try again later' />;
   }
 
   return (
